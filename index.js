7a78dded49d125d0b4dfcc9adcb84e15518fda34
adamgajzlerowicz <nelf86@gmail.com>
2018-04-06 06:12:55 +0200

    push

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const S = require('sanctuary');
 
-const log = x => {
-    console.log(x);
-    return x;
-}
 const a = 1;
 const b = {b: 'b'};
 const c = null;
 const d = null;
 
-const inc = x => {
-    return x + 1;
-};
+const inc = x => x + 1;
 
-const checkIsTrue = S.and(S.isJust(S.toMaybe(a)), S.isJust(S.toMaybe(b)));
-const checkIsFalse = S.and(S.isJust(S.toMaybe(c)), S.isJust(S.toMaybe(d)));
+const isTrue = S.and(S.isJust(S.toMaybe(a)), S.isJust(S.toMaybe(b)));
+const isFalse = S.and(S.isJust(S.toMaybe(c)), S.isJust(S.toMaybe(d)));
 
 const result = S.pipe([
     S.map(inc),
     S.eitherToMaybe,
-    log,
-], checkIsTrue ? S.Right(a) : S.Left(b));
+    S.maybeToNullable
+], isFalse ? S.Right(a) : S.Left(a));
 
 
-console.log(S.fromMaybe(null, S.Just(42)));
-// console.log(result);
+console.log(result);
 
 
